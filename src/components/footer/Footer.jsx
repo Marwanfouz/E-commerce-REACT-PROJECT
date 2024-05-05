87510bb26e734ed173c7ff6033d1959b27873b50
Marwan Fouz <marwanfouz76@gmail.com>
2024-05-05 20:41:08 +0300

    feat: footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const date = new Date();
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer>
+      <span>
+        &copy;{`${date.getFullYear()}`}
+        <Link target="_blank" to={"mailto: marwanfouz76@gmail.com"}>
+          Marwan Fouz .
+        </Link>
+        All Reversed.
+      </span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
